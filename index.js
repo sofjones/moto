ba60f8ae62eb0b75506d00151dd551e8382e021a
sofjones <sofiaj611@gmail.com>
2022-04-24 17:04:13 -0700

    basic CRUD functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 
 const hostname = 'localhost';
@@ -8,8 +9,51 @@ const port = 3000;
 
 const app = express();
 
-app.use(express.static(__dirname+ '/public'));
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/switches', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+
+});
+
+app.get('/switches', (req, res, next) => {
+    res.end('Will send all switches to you!');
+});
+
+app.post('/switches', (req, res, next) => {
+    res.end('Will add the switch: ' + req.body.switchId)
+});
+
+app.put('/switches', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/switches', (req, res, next) => {
+    res.destroy('Delete errything');
+});
+
+app.get('/switches/:switchId', (req, res, next) => {
+    res.end('Will send details of switch: ' + req.params.switchId);
+});
+
+app.post('/switches/:switchId', (req, res, next) => {
+    req.statusCode = 403;
+    res.end('POST not supported on specific switch')
+});
+
+app.put('/switches/:switchId', (req, res, next) => {
+    res.end('Will update params on switch' + req.params.switchId);
+});
+
+app.delete('/switches:switchId', (req, res, next) => {
+    res.destroy('Delete switch with id' + req.params.switchId);
+});
+app.use(express.static(__dirname+ '/public'));
+
 app.use((req, res, next) => {
     //console.log(req.headers); //remove as morgan will log
     res.statusCode=200;
